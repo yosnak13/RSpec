c907d8426a60d92d0aaaf184481ce3767685646d
“yosnak13” <yosnak13@gmail.com>
2020-10-13 23:32:00 +0900

    3-13 12, 13はエラーが治らない

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -27,62 +27,6 @@ RSpec.describe FoodEnquete, type: :model do
     end
   end
 
-  describe '入力項目の有無' do
-    let(:new_enquete) { FoodEnquete.new }
-    context '必須入力であること' do
-      # [Point.3-4-1]itを複数書くことができます。
-      it 'お名前が必須であること' do
-        # [Point.3-4-2]バリデーションエラーが発生することを検証します。
-        expect(new_enquete).not_to be_valid
-        # [Point.3-4-3]必須入力のメッセージが含まれることを検証します。
-        expect(new_enquete.errors[:name]).to include(I18n.t('errors.messages.blank'))
-      end
-
-      it 'メールアドレスが必須であること' do
-        expect(new_enquete).not_to be_valid
-        expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.blank'))
-      end
-      # [Point.3-4-1]itを複数書くことができます。
-      it '登録できないこと' do
-        # [Point.3-4-4]保存に失敗することを検証します。
-        expect(new_enquete.save).to be_falsey
-      end
-    end
-    context '任意入力であること' do
-    it 'ご意見・ご要望が任意であること' do
-      expect(new_enquete).not_to be_valid
-      expect(new_enquete.errors[:request]).not_to include(I18n.t('errors.messages.blank'))
-    end
-  end
-  end
-
-  describe '#adult?' do
-    it '20歳未満は成人ではないこと' do
-      foodEnquete = FoodEnquete.new
-      # [Point.3-5-1]未成年になることを検証します。
-      expect(foodEnquete.send(:adult?, 19)).to be_falsey
-    end
-
-    it '20歳以上は成人であること' do
-      foodEnquete = FoodEnquete.new
-      # [Point.3-5-2]成人になることを検証します。
-      expect(foodEnquete.send(:adult?, 20)).to be_truthy
-    end
-  end
-
-  describe 'メールアドレスの形式' do
-    context '不正な形式のメールアドレスの場合' do
-      it 'エラーになること' do
-        new_enquete = FoodEnquete.new
-        # [Point.3-7-1]不正な形式のメールアドレスを入力します。
-        new_enquete.mail = "taro.tanaka"
-        expect(new_enquete).not_to be_valid
-        # [Point.3-7-2]不正な形式のメッセージが含まれることを検証します。
-        expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.invalid'))
-      end
-    end
-  end
-
   describe 'アンケート回答時の条件' do
     context 'メールアドレスを確認すること' do
       before do
@@ -118,5 +62,16 @@ RSpec.describe FoodEnquete, type: :model do
         expect(enquete_sato).to be_valid
       end
     end
+  end
+
+  describe '共通バリデーション' do
+    it_behaves_like '入力項目の有無'
+    it_behaves_like 'メールアドレスの形式'
+  end
+  
+  describe '共通メソッド' do
+    # [Point.3-12-3]共通化するテストケースを定義します。
+    it_behaves_like '価格の表示'
+    it_behaves_like '満足度の表示'
   end
 end
--- spec/models/gym_enquete_spec.rb
+++ spec/models/gym_enquete_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+ require 'rails_helper'
 
-RSpec.describe GymEnquete, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ RSpec.describe GymEnquete, type: :model do
+  # ==========ここから削除する==========
+  # 【補足】雛型で生成されたテストケースは不要です。
+  # pending "add some examples to (or delete) #{__FILE__}"
+  # ==========ここまで削除する==========
+  describe '共通バリデーション' do
+    it_behaves_like '入力項目の有無'
+    it_behaves_like 'メールアドレスの形式'
+  end
+  # ==========ここから追加する==========
+  describe '共通メソッド' do
+    it_behaves_like '価格の表示'
+    it_behaves_like '満足度の表示'
+  end
+  # ==========ここまで追加する==========
+
 end
